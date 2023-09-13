a3f9794c84c8cd4d6b24d9ab0f7b8e5a37a2d09d
Mohsan Ali <52249539+mohsiiawan@users.noreply.github.com>
2023-09-13 23:26:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+var maincontainer = document.querySelector('.container');
+var progressbar = document.getElementById('progress');
 var sections = document.querySelectorAll('#section');
 var windowHeight = window.innerHeight;
-var scrollThreshold = 40;
-
+var scrollThreshold = 40;  
 window.addEventListener('scroll', () => {
+    var containerheight = maincontainer.clientHeight;
+    var containertop = maincontainer.getBoundingClientRect().top;
+    console.log("top is " + containertop);
+    var a = (containerheight + containertop) ;
+    var b = (a/containerheight) * 100;
+    var progressbarheight =(100 - b);
+    if (containertop <= "-"+containerheight){
+        console.log(containerheight);
+        progressbar.style.height = '100%'
+    }
+    else if(containertop <= 0){
+        console.log("progress in " + b);
+        progressbar.style.height =`${progressbarheight}%`;
+    }
     for (var i = 0; i < sections.length; ++i) {
         var rect = sections[i].getBoundingClientRect();
         var topPercentage = (rect.y / windowHeight) * 100;
